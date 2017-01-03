805b2aebd4f1757184d48a0afb7c712905cf1257
Ameen Demidem <ameen.demidem@gmail.com>
2017-01-02 21:39:00 -0500

    Add hashing and retrieving of shortened url

--- server.js
+++ server.js
@@ -7,29 +7,52 @@ const dbURL = 'mongodb://localhost:27017/free_code_camp';
 
 app
   .set('port', process.env.PORT || 3000)
-  .get('/:url', (req,res) => {
-    mongo.connect(dbURL, (err, db) => {
-      let errMsg;
 
-      if (err) {
-        errMsg = 'Couldn\'t connect to the database!';
-      } else {
-        result = db.collection('shortened_urls').insert({url: req.params.url});
-        if (result.writeError) {
-          errMsg = 'Couldn\'t write to the database!';
-        }
-      }
+  .get('/new/:url', (req,res) => mongo.connect(dbURL)
+    .then(db => db.collection('shortened_urls').insert({
+      url: req.params.url,
+      shortened: shortenUrl(req.params.url)
+    }))
+    .then(result => {
+      if (result.writeError) return Promise.reject(result.writeError.errmsg);
+      res.end(JSON.stringify({
+        url: result.ops[0].url, short_url: result.ops[0].shortened
+      }));
+    })
+    .catch(err => {
+      res.writeHead(500);
+      console.error('Something went wrong:', err);
+      res.end(err);
+    })
+  )
 
-      if (errMsg) {
-        res.writeHead(500);
-        console.error('Something went wrong:', errMsg);
-      } else {
-        console.log('Successfully inserted url', req.params.url);
-      }
+  .get('/:shortURL', (req, res) => mongo.connect(dbURL)
+    .then(db => db.collection('shortened_urls').findOne({
+      shortened: req.params.shortURL
+    }))
+    .then(document => {
+      if (!document) return Promise.reject('Couldn\'t find the requested url!');
+      console.log('Successfully fetched url', document.url);
+      res.end(document.url);
+    })
+    .catch(err => {
+      res.writeHead(500);
+      console.error('Something went wrong:', err);
+      res.end(err);
+    })
+  )
 
-      res.end(errMsg || 'You\'re good!');
-    });
-  })
   .listen(app.get('port'));
 
 console.log('Listening on port', app.get('port'));
+
+function shortenUrl(url) {
+  let hash = 0, chr;
+  if (url.length === 0) return hash;
+  for (i = 0; i < url.length; i++) {
+    chr = url.charCodeAt(i);
+    hash = ((hash<<5)-hash)+chr;
+    hash &= hash; // Convert to 32bit integer
+  }
+  return (hash >>> 0).toString(16);
+}
